8a62d3575196e6b352c766775610ce8db7d86137
Dishant <koratdishant536631@gmail.com>
2023-04-19 22:01:59 +0530

    implemented local server

--- app.py
+++
@@ -1,4 +0,0 @@
-from dkrespar import extract_data
-
-# print(extract_data("https://drive.google.com/file/d/1U0lGu-fa7B7BlSCEZOShAph8U6p3zC7B/view"))
-print(extract_data("https://docs.google.com/document/d/13wh5v5_5eSQH6-OMVaNC4T2KfqJKYviJdi5R-RDftZI/edit"))
